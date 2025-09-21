710b9e5704a69d8fd70a3deebc0cf99fbb6afe6c
wildan-pcr <wildan24ti@mahasiswa.pcr.ac.id>
2025-09-21 16:45:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -2,26 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\MahasiswaController;
-
-Route::get('/pcr', function () {
- return 'Selamat Datang di Website Kampus PCR!';});
-
- Route::get('/nama/{param1}', function ($param1) {
-    return 'Nama saya: '.$param1;
-});
-
-Route::get('/about', function () {
-    return view('halaman-about');
-});
-
-Route::get('/mahasiswa', function () {
-    return 'Halo Mahasiswa';
-})->name('mahasiswa.show');
-
-route :: get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
-
-
 Route::get('/about-buah', function () {
     return view('halaman-about-jus-buah');
 });
